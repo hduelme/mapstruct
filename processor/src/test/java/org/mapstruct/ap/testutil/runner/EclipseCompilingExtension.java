3486f51586168a7ffb944f57b8cc45817e2683fe
hduelme <hduelme@gmx.de>
2026-02-06 12:03:39 +0100

    Update tycho-compiler-jdt to 2.0.0

--- processor/src/test/java/org/mapstruct/ap/testutil/runner/EclipseCompilingExtension.java
+++ processor/src/test/java/org/mapstruct/ap/testutil/runner/EclipseCompilingExtension.java
@@ -9,7 +9,10 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 import javax.lang.model.SourceVersion;
@@ -121,6 +124,9 @@ class EclipseCompilingExtension extends CompilingExtension {
     }
 
     public static final class ClassLoaderExecutor implements ClassLoaderHelper {
+
+        private static final Map<String, String> ECLIPSE_SUPPRESS_ALL_WARNINGS = buildEclipseSuppressAllWarnings();
+
         @Override
         public CompilationOutcomeDescriptor compileInOtherClassloader(CompilationRequest compilationRequest,
                                                                       List<String> testCompilationClasspath,
@@ -139,7 +145,8 @@ class EclipseCompilingExtension extends CompilingExtension {
             config.setAnnotationProcessors( new String[] { MappingProcessor.class.getName() } );
             config.setSourceFiles( sourceFiles );
             String version = getSourceVersion();
-            config.setShowWarnings( false );
+            config.setShowWarnings( true );
+            config.setCustomCompilerArgumentsAsMap( ECLIPSE_SUPPRESS_ALL_WARNINGS );
             config.setSourceVersion( version );
             config.setTargetVersion( version );
 
@@ -166,6 +173,98 @@ class EclipseCompilingExtension extends CompilingExtension {
                 return "1.8";
             }
             return "11";
+        }
+
+        private static Map<String, String> buildEclipseSuppressAllWarnings() {
+            Map<String, String> suppressWarnings = new HashMap<>();
+            suppressWarnings.put( "-warn:-all-static-method", null );
+            suppressWarnings.put( "-warn:-allDeprecation", null );
+            suppressWarnings.put( "-warn:-allJavadoc", null );
+            suppressWarnings.put( "-warn:-assertIdentifier", null );
+            suppressWarnings.put( "-warn:-boxing", null );
+            suppressWarnings.put( "-warn:-charConcat", null );
+            suppressWarnings.put( "-warn:-compareIdentical", null );
+            suppressWarnings.put( "-warn:-conditionAssign", null );
+            suppressWarnings.put( "-warn:-constructorName", null );
+            suppressWarnings.put( "-warn:-deadCode", null );
+            suppressWarnings.put( "-warn:-dep-ann", null );
+            suppressWarnings.put( "-warn:-discouraged", null );
+            suppressWarnings.put( "-warn:-emptyBlock", null );
+            suppressWarnings.put( "-warn:-enumIdentifier", null );
+            suppressWarnings.put( "-warn:-enumSwitch", null );
+            suppressWarnings.put( "-warn:-enumSwitchPedantic", null );
+            suppressWarnings.put( "-warn:-exports", null );
+            suppressWarnings.put( "-warn:-fallthrough", null );
+            suppressWarnings.put( "-warn:-fieldHiding", null );
+            suppressWarnings.put( "-warn:-finalBound", null );
+            suppressWarnings.put( "-warn:-finally", null );
+            suppressWarnings.put( "-warn:-forbidden", null );
+            suppressWarnings.put( "-warn:-hashCode", null );
+            suppressWarnings.put( "-warn:-hiding", null );
+            suppressWarnings.put( "-warn:-includeAssertNull", null );
+            suppressWarnings.put( "-warn:-indirectStatic", null );
+            suppressWarnings.put( "-warn:-inheritNullAnnot", null );
+            suppressWarnings.put( "-warn:-interfaceNonInherited", null );
+            suppressWarnings.put( "-warn:-intfAnnotation", null );
+            suppressWarnings.put( "-warn:-intfNonInherited", null );
+            suppressWarnings.put( "-warn:-invalidJavadoc", null );
+            suppressWarnings.put( "-warn:-javadoc", null );
+            suppressWarnings.put( "-warn:-localHiding", null );
+            suppressWarnings.put( "-warn:-maskedCatchBlock", null );
+            suppressWarnings.put( "-warn:-maskedCatchBlocks", null );
+            suppressWarnings.put( "-warn:-missingJavadocComments", null );
+            suppressWarnings.put( "-warn:-missingJavadocTags", null );
+            suppressWarnings.put( "-warn:-module", null );
+            suppressWarnings.put( "-warn:-nls", null );
+            suppressWarnings.put( "-warn:-noEffectAssign", null );
+            suppressWarnings.put( "-warn:-noImplicitStringConversion", null );
+            suppressWarnings.put( "-warn:-nonnullNotRepeated", null );
+            suppressWarnings.put( "-warn:-null", null );
+            suppressWarnings.put( "-warn:-nullAnnotConflict", null );
+            suppressWarnings.put( "-warn:-nullAnnotRedundant", null );
+            suppressWarnings.put( "-warn:-nullDereference", null );
+            suppressWarnings.put( "-warn:-nullUncheckedConversion", null );
+            suppressWarnings.put( "-warn:-over-ann", null );
+            suppressWarnings.put( "-warn:-over-sync", null );
+            suppressWarnings.put( "-warn:-packageDefaultMethod", null );
+            suppressWarnings.put( "-warn:-paramAssign", null );
+            suppressWarnings.put( "-warn:-pkgDefaultMethod", null );
+            suppressWarnings.put( "-warn:-raw", null );
+            suppressWarnings.put( "-warn:-redundantSuperinterface", null );
+            suppressWarnings.put( "-warn:-resource", null );
+            suppressWarnings.put( "-warn:-semicolon", null );
+            suppressWarnings.put( "-warn:-serial", null );
+            suppressWarnings.put( "-warn:-specialParamHiding", null );
+            suppressWarnings.put( "-warn:-static-access", null );
+            suppressWarnings.put( "-warn:-staticReceiver", null );
+            suppressWarnings.put( "-warn:-super", null );
+            suppressWarnings.put( "-warn:-suppress", null );
+            suppressWarnings.put( "-warn:-switchDefault", null );
+            suppressWarnings.put( "-warn:-syncOverride", null );
+            suppressWarnings.put( "-warn:-syntacticAnalysis", null );
+            suppressWarnings.put( "-warn:-synthetic-access", null );
+            suppressWarnings.put( "-warn:-syntheticAccess", null );
+            suppressWarnings.put( "-warn:-typeHiding", null );
+            suppressWarnings.put( "-warn:-unavoidableGenericProblems", null );
+            suppressWarnings.put( "-warn:-unchecked", null );
+            suppressWarnings.put( "-warn:-unlikelyCollectionMethodArgumentType", null );
+            suppressWarnings.put( "-warn:-unlikelyEqualsArgumentType", null );
+            suppressWarnings.put( "-warn:-unnecessaryElse", null );
+            suppressWarnings.put( "-warn:-unqualified-field-access", null );
+            suppressWarnings.put( "-warn:-unqualifiedField", null );
+            suppressWarnings.put( "-warn:-unsafe", null );
+            suppressWarnings.put( "-warn:-unused", null );
+            suppressWarnings.put( "-warn:-unusedParamImplementing", null );
+            suppressWarnings.put( "-warn:-unusedParamIncludeDoc", null );
+            suppressWarnings.put( "-warn:-unusedParamOverriding", null );
+            suppressWarnings.put( "-warn:-unusedThrownExemptExceptionThrowable", null );
+            suppressWarnings.put( "-warn:-unusedThrownIncludeDocComment", null );
+            suppressWarnings.put( "-warn:-unusedThrownWhenOverriding", null );
+            suppressWarnings.put( "-warn:-unusedTypeParameter", null );
+            suppressWarnings.put( "-warn:-uselessTypeCheck", null );
+            suppressWarnings.put( "-warn:-varargsCast", null );
+            suppressWarnings.put( "-warn:-warningToken", null );
+            return Collections.unmodifiableMap( suppressWarnings );
         }
 
     }
