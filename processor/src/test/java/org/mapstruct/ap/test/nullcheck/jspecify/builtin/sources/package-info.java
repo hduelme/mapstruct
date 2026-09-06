d4992c1350e3eaa434116c7d5db528ecd584d82e
hduelme <hduelme@gmx.de>
2026-09-06 21:43:36 +0200

    add licence

--- processor/src/test/java/org/mapstruct/ap/test/nullcheck/jspecify/builtin/sources/package-info.java
+++ processor/src/test/java/org/mapstruct/ap/test/nullcheck/jspecify/builtin/sources/package-info.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright MapStruct Authors.
+ *
+ * Licensed under the Apache License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
+ */
 @NullMarked
 package org.mapstruct.ap.test.nullcheck.jspecify.builtin.sources;
 
