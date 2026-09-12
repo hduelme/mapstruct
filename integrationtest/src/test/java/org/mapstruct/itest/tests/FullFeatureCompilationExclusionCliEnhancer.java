293da975c9e34492fbbe8282f2a67a6c2d716a45
hduelme <hduelme@gmx.de>
2026-09-12 13:20:31 +0200

    format

--- integrationtest/src/test/java/org/mapstruct/itest/tests/FullFeatureCompilationExclusionCliEnhancer.java
+++ integrationtest/src/test/java/org/mapstruct/itest/tests/FullFeatureCompilationExclusionCliEnhancer.java
@@ -55,9 +55,7 @@ public final class FullFeatureCompilationExclusionCliEnhancer implements Process
                 // JVMs pre 22 have problems reading type in use annotations
                 // Here Java 8 does not see the nested class annotation
                 // https://github.com/jspecify/jspecify/wiki/version-compatibility#issues-with-javac-before-version-22
-                additionalExcludes.add(
-                        "org/mapstruct/ap/test/bugs/_4086/*"
-                );
+                additionalExcludes.add( "org/mapstruct/ap/test/bugs/_4086/*" );
                 break;
             case JAVA_11:
                 additionalExcludes.add( "org/mapstruct/ap/test/**/spring/**/*.java" );
